8ea581e2185e6389ddcd9c5106cd6ffd85f24f5e
sylvester976 <officialotienosylvester@gmail.com>
2025-11-21 15:57:09 +0300

    solving tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -7,7 +7,12 @@ test('guests are redirected to the login page', function () {
 });
 
 test('authenticated users can visit the dashboard', function () {
-    $this->actingAs($user = User::factory()->create());
+    // Create a verified user
+    $user = User::factory()->create([
+        'email_verified_at' => now(), // mark email as verified
+    ]);
+
+    $this->actingAs($user);
 
     $this->get('/dashboard')->assertStatus(200);
 });
